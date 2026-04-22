bf0ae4e9eb1535731b353c62e6856195eb23c4e4
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-04-22 19:32:55 +0500

    count

--- guest/guestJS/main.js
+++ guest/guestJS/main.js
@@ -7,3 +7,14 @@ async function three() {
     render(data.slice(0,3));
 }
 three()
+
+
+
+let all = document.querySelector(".all")
+all.onclick=()=>{
+    window.location.href="/guest/guestmain/main.html"
+}
+let show = document.querySelector(".show")
+show.onclick=()=>{
+    window.location.href="/guest/guestmain/main.html"
+}
